1cb4654d6bae0d3458b30e9240f3fede7389dacc
CuonoMaione <cuonomaione99@outlook.it>
2023-06-30 17:16:28 +0200

    refactoring

--- server.php
+++ server.php
@@ -3,9 +3,24 @@
 $text = $_GET['text'];
 $badword = $_GET['badword'];
 
-
-echo $text . '  ' . strlen($text);
-
-echo str_replace($badword,'REPLACED',$text)
-
 ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1><?php echo $text . '  ' . strlen($text); ?></h1>
+
+    <h2><?php echo str_replace ($badword,'REPLACED',$text); ?></h2>
+</body>
+</html>
+
+
+
+
+
